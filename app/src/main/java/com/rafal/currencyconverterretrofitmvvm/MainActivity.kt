eb627dfa8616ef98c83eacbcd8762cb134ec67cb
Rafal <rafalswiech3@gmail.com>
2021-07-01 14:35:26 +0200

    Empty project

---
+++ app/src/main/java/com/rafal/currencyconverterretrofitmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rafal.currencyconverterretrofitmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
